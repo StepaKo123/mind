ad5b3e42875a3cc634ed368c1421ed494eed4aec
Stepan Kozhevnikov <stepa.kozhevnikov@mail.ru>
2024-03-13 18:45:41 +0300

    try

--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path("", views.index)
+    path("", views.main1, name="main"),
+    path("/tutors", views.tutors, name="tutors"),
+    path("/index", views.index, name="index")
 ]
--- main/views.py
+++ main/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
 
+def main1(request):
+    return render(request, 'main/main.html')
+
+def tutors(request):
+    return render(request, "main/tutors.html")
+
 def index(request):
-    return render(request, 'main/index.html')
+    return render(request, "main/index.html")
